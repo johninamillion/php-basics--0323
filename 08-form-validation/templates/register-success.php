b20da5d69ee50c4f46ed3960e62fd125b25c79aa
johninamillion <johninamio@gmail.com>
2023-06-26 16:34:33 +0200

    create template files

---
+++ 08-form-validation/templates/register-success.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+	<meta charset="UTF-8">
+	<title>Form Success</title>
+</head>
+<body>
+<main>
+	<p>Deine Registrierung war erfolgreich!</p>
+</main>
+</body>
+</html>
