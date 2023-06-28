1ce744b185fcfef8a06d600c8fb7c243e552d207
johninamillion <johninamio@gmail.com>
2023-06-28 15:51:06 +0200

    create script

---
+++ 12-gettext/bin/run.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace SaeInstitute\PhpGetText;
+
+use Gettext\Generator\MoGenerator;
+use Gettext\Generator\PoGenerator;
+use Gettext\Loader\PoLoader;
+use Gettext\Scanner\PhpScanner;
+use Gettext\Translations;
+
+require_once  __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+
+$poLoader = new PoLoader();
+$previousTranslations = $poLoader->loadFile( 'locales/en.po' );
+
+$phpScanner = new PhpScanner(
+	Translations::create( 'domain1' )
+);
+
+$phpScanner->setDefaultDomain( 'domain1' );
+
+$phpScanner->extractCommentsStartingWith( 'i18n:' );
+
+foreach( glob( __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '**/*.php' ) as $file ) {
+	$phpScanner->scanFile( $file );
+}
+
+foreach( $phpScanner->getTranslations() as $translation ) {
+	$previousTranslations = $previousTranslations->mergeWith( $translation );
+}
+
+$poGenerator = new PoGenerator();
+$poGenerator->generateFile( $previousTranslations, 'locales/en.po' );
+
+$moGenerator = new MoGenerator();
+$moGenerator->generateFile( $previousTranslations, 'locales/en.mo' );
+
+
