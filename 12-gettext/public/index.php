1da00ee92c131711ff79c14e01f97e75fce8ff6d
johninamillion <johninamio@gmail.com>
2023-06-28 15:12:28 +0200

    require autoloader

---
+++ 12-gettext/public/index.php
@@ -0,0 +1,5 @@
+<?php
+
+// autoload von composer einbinden
+// alle Class, Interface, Trait, Enum, etc. werden automatisch geladen
+require_once  __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
